744f8c1bebeb190ad40e8629bd95092259ea99c0
Antoine Toussaint <antoine@antoines-mbp-2.myfiosgateway.com>
2023-09-12 14:04:49 +0200

    first commit

---
+++ render.go
@@ -0,0 +1,249 @@
+package render
+
+import (
+	"fmt"
+	"github.com/fatih/color"
+	"regexp"
+	"strings"
+)
+
+type Renderer struct {
+	theme   Theme
+	scanner *Scanner
+}
+
+func (renderer *Renderer) Render(text string) string {
+	tokens := renderer.scanner.Scan(text)
+	var rendered []string
+	for _, token := range tokens {
+		rendered = append(rendered, renderer.theme.Convert(token))
+	}
+	return strings.Join(rendered, "")
+}
+
+func NewRenderer() *Renderer {
+	return &Renderer{
+		theme:   Theme{},
+		scanner: NewScanner(),
+	}
+}
+
+type Scanner struct {
+	TagMarker int32
+	Start     int32
+	End       int32
+}
+
+func NewScanner() *Scanner {
+	return &Scanner{TagMarker: '#', Start: '{', End: '}'}
+}
+
+type Token struct {
+	Text  string
+	Style *Style
+}
+
+type Type interface {
+}
+
+const (
+	Default = iota
+	Red
+	Green
+)
+
+type Color struct {
+	value int
+}
+
+const (
+	None = iota
+	Bold
+	Italic
+)
+
+type Typography struct {
+	value int
+}
+
+type Style struct {
+	color        Color
+	typographies []Typography
+}
+
+func (style *Style) Clone() *Style {
+	s := Style{
+		color: style.color,
+	}
+	copy(s.typographies, style.typographies)
+	return &s
+}
+
+func (style *Style) Add(ts ...string) {
+	for _, t := range ts {
+		style.AddOne(t)
+	}
+}
+
+func (style *Style) String() string {
+	return fmt.Sprintf("color: %d, typographies: %v", style.color, style.typographies)
+}
+
+func NewStyle() *Style {
+	return &Style{}
+}
+
+func SameStyle(a, b *Style) bool {
+	if a == nil && b == nil {
+		return true
+	}
+	if a != nil && b == nil {
+		return false
+	}
+	if a == nil && b != nil {
+		return false
+	}
+	if a.color != b.color {
+		return false
+	}
+	if len(a.typographies) != len(b.typographies) {
+		return false
+	}
+	match := make(map[int]bool)
+	for _, t := range a.typographies {
+		match[t.value] = true
+	}
+	for _, t := range b.typographies {
+		if !match[t.value] {
+			return false
+		}
+	}
+	return true
+}
+
+func (style *Style) Color(color int) *Style {
+	style.color = Color{value: color}
+	return style
+}
+
+func (style *Style) Typography(t int) *Style {
+	existing := make(map[int]bool)
+	for _, t := range style.typographies {
+		existing[t.value] = true
+	}
+	if !existing[t] {
+		style.typographies = append(style.typographies, Typography{value: t})
+	}
+	return style
+}
+
+func (style *Style) AddOne(t string) {
+	switch t {
+	case "red":
+		style.Color(Red)
+	case "bold":
+		style.Typography(Bold)
+	case "italic":
+		style.Typography(Italic)
+	}
+}
+
+type Theme struct {
+}
+
+func (theme *Theme) Color(c Color) (color.Attribute, bool) {
+	switch c.value {
+	case Red:
+		return color.FgRed, true
+	default:
+		return 0, false
+	}
+}
+
+func (theme *Theme) Typographies(typographies []Typography) []color.Attribute {
+	var attributes []color.Attribute
+	for _, t := range typographies {
+		switch t.value {
+		case Bold:
+			attributes = append(attributes, color.Bold)
+		case Italic:
+			attributes = append(attributes, color.Italic)
+		}
+	}
+	return attributes
+}
+
+func (theme *Theme) Convert(token Token) string {
+	// Use fatih/color
+	if token.Style == nil {
+		return token.Text
+	}
+	var attributes []color.Attribute
+	if c, ok := theme.Color(token.Style.color); ok {
+		attributes = append(attributes, c)
+	}
+	attributes = append(attributes, theme.Typographies(token.Style.typographies)...)
+	f := color.New(attributes...).SprintFunc()
+	return f(token.Text)
+}
+
+func (s *Scanner) Scan(text string) []Token {
+	var tokens []Token
+	var currentText string
+	var currentTags string
+	var insideTag bool
+
+	styles := make(map[int]*Style)
+	level := 0
+	reg := regexp.MustCompile(`\(?([\w,]*)\)?`)
+
+	for _, char := range text {
+		switch char {
+		case s.TagMarker:
+			token := Token{Text: currentText, Style: styles[level]}
+			tokens = append(tokens, token)
+			insideTag = true
+			currentText = ""
+		case s.Start:
+			match := reg.Match([]byte(currentTags))
+			if !match {
+				panic(fmt.Sprintf("invalid tag: %s", currentTags))
+			}
+			currentTags = reg.FindStringSubmatch(currentTags)[1]
+			tags := strings.Split(currentTags, ",")
+			previous := styles[level]
+			level += 1
+			next := NewStyle()
+			if previous != nil {
+				next = previous.Clone()
+			}
+			next.Add(tags...)
+			styles[level] = next
+			currentTags = ""
+			insideTag = false
+		case s.End:
+			if len(currentText) > 0 {
+				token := Token{Text: currentText, Style: styles[level]}
+				tokens = append(tokens, token)
+			}
+			currentText = ""
+			level -= 1
+
+		default:
+			if insideTag {
+				currentTags += string(char)
+			} else {
+				currentText += string(char)
+			}
+		}
+	}
+	if level != 0 {
+		panic(fmt.Sprintf("invalid number of tags: %d", level))
+	}
+
+	// Add the last token
+	if len(currentText) > 0 {
+		tokens = append(tokens, Token{Text: currentText, Style: styles[level]})
+	}
+	return tokens
+}
---
+++ render_test.go
@@ -0,0 +1,36 @@
+package render_test
+
+import (
+	render "github.com/hygge-io/color"
+	"testing"
+)
+
+func TestScanner(t *testing.T) {
+	scanner := render.NewScanner()
+	tcs := []struct {
+		name string
+		text string
+		want []render.Token
+	}{
+		{name: "no rendering", text: "no rendering", want: []render.Token{{Text: "no rendering"}}},
+		{name: "simple", text: "this #red{cat is colorful} and this is not", want: []render.Token{{Text: "this "}, {Text: "cat is colorful", Style: render.NewStyle().Color(render.Red)}, {Text: " and this is not"}}},
+		{name: "two tags", text: "this #(red,bold){cat is colorful and awesome} and this is not", want: []render.Token{{Text: "this "}, {Text: "cat is colorful and awesome", Style: render.NewStyle().Color(render.Red).Typography(render.Bold)}, {Text: " and this is not"}}},
+		{name: "nested", text: "this #red{cat is #bold{awesome}}", want: []render.Token{{Text: "this "}, {Text: "cat is ", Style: render.NewStyle().Color(render.Red)}, {Text: "awesome", Style: render.NewStyle().Color(render.Red).Typography(render.Bold)}}},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			got := scanner.Scan(tc.text)
+			if len(got) != len(tc.want) {
+				t.Errorf("got %d tokens, want %d", len(got), len(tc.want))
+			}
+			for i, g := range got {
+				if g.Text != tc.want[i].Text {
+					t.Errorf("Text not matching, got <%v>, want <%v>", g.Text, tc.want[i].Text)
+				}
+				if !render.SameStyle(g.Style, tc.want[i].Style) {
+					t.Errorf("Style not matching for token <%s>, got %v, want %v", g.Text, g.Style, tc.want[i].Style)
+				}
+			}
+		})
+	}
+}
